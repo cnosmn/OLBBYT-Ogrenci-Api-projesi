6e7224a199cb2c4d2df2a47ccd9982eef960a419
Osman Can <cnosman14043@gmail.com>
2026-05-10 14:58:08 +0300

    ogrenci güncelleme ekleme eklendi

--- src/controllers/ogrenci.controller.ts
+++ src/controllers/ogrenci.controller.ts
@@ -20,7 +20,6 @@ export const tumOgrencileriGetir = (
     });
 };
 
-
 // GET /api/ogrenciler/:id
 
 export const ogrenciGetir = (
@@ -45,3 +44,72 @@ export const ogrenciGetir = (
 };
 
 
+// POST /api/ogrenciler
+export const ogrenciEkle = (
+    req:Request,
+    res:Response
+): void => {
+    const yeniOgrenciVerisi : OgrenciOlusturmaDTO =req.body;
+    if (!yeniOgrenciVerisi ||
+        !yeniOgrenciVerisi.ad || 
+        !yeniOgrenciVerisi.soyad || 
+        !yeniOgrenciVerisi.sinif ||
+        !yeniOgrenciVerisi.yas ||
+        !yeniOgrenciVerisi.okul ||
+        !yeniOgrenciVerisi.notOrtalamasi
+    ) {
+        res.status(400).json({
+        success:false,
+        error:"tüm alanları doldurunuz (ad,soyad,sinif,yas,okul,notOrtalamasi)",
+    });
+    return;
+    }
+    //https://codeshare.io/GqrPQR
+    
+    const yeniOgrenci : Ogrenci = {
+        id:(ogrenciler.length + 1).toString(),
+        ad:yeniOgrenciVerisi.ad,
+        soyad:yeniOgrenciVerisi.soyad,
+        sinif:yeniOgrenciVerisi.sinif,
+        okul:yeniOgrenciVerisi.okul,
+        notOrtalamasi:yeniOgrenciVerisi.notOrtalamasi,
+        olusturma_tarihi:new Date(),
+        yas:yeniOgrenciVerisi.yas
+    }
+
+    //ad:yeniOgrenciVerisi.ad,
+    //soyad:yeniOgrenciVerisi.soyad,
+    //sinif:yeniOgrenciVerisi.sinif,
+    //okul:yeniOgrenciVerisi.okul,
+    //notOrtalamasi:yeniOgrenciVerisi.notOrtalamasi,
+
+    // yerine ...yeniOgrenciVerisi yazabiliriz
+
+    ogrenciler.push(yeniOgrenci);
+
+    res.status(201).json({
+        success:true,
+        data:yeniOgrenci,
+    });
+};
+
+
+// PUT /api/ogrenciler/:id
+
+export const ogrenciGuncelle = (
+    req:Request,
+    res:Response
+): void => {
+    const {id} = req.params;
+    const guncellenecekVeri : OgrenciGuncellemeDTO = req.body;
+
+    const index = ogrenciler.findIndex((o) => o.id === id);
+    // index -1 değerde ise o id'ye sahip öğrenci yok demek
+    
+
+    res.status(200).json({
+        success:true,
+        data:ogrenciler,
+        count:ogrenciler.length,
+    });
+};
--- src/routes/ogrenci.routes.ts
+++ src/routes/ogrenci.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
-import { tumOgrencileriGetir } from '../controllers/ogrenci.controller';
+import { ogrenciEkle, tumOgrencileriGetir } from '../controllers/ogrenci.controller';
 import { ogrenciGetir } from '../controllers/ogrenci.controller';
 const router = Router();
 
 router.get("/",tumOgrencileriGetir);
 router.get("/:id",ogrenciGetir);
+router.post("/",ogrenciEkle);
+
 
 export default router;
